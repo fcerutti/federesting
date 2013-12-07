737c1222e3e51cee3f76e1e295e0e402894b32c2
Fede Cerutti <fedecerutti@gmail.com>
2013-12-07 18:48:49 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Federesting::Application.config.secret_key_base = '9a2eea6c336b67447b586152c0f863c5183520ab34afad250f1578cb9957ab7c8f3255be304ec0d223e4e4cdf5bfd61f983195e3212689de5326d010d56bf2b5'
